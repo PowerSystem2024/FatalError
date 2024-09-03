a3c14bb7fbefe29484a57efdf440f247fe9b2a02
Luca Perez <lucamarper@gmail.com>
2024-09-03 11:44:45 -0300

    Vimos comienzo de funciones y la palabra return

---
+++ LucaPerez/javascript/TSLeccion03/01-03-Funciones.js
@@ -0,0 +1,13 @@
+miFuncion(8,2); //esto se le conoce como hosting
+
+function    miFuncion(a,b){
+    //console.log("Sumamos: " + (a+b));
+    return a+b;
+}
+
+//llamada a la función
+miFuncion(5,4);
+
+let resultado = miFuncion(6,7);
+console.log(resultado);
+
