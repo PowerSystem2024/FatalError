ed88c993d7bc42921aad41dd9278d4d00995c056
Luca Perez <lucamarper@gmail.com>
2024-09-03 11:53:32 -0300

    Vimos la funcion de tipo expresion y la funciones de tipo self y invoking

--- LucaPerez/javascript/TSLeccion03/01-03-Funciones.js
+++ LucaPerez/javascript/TSLeccion03/01-03-Funciones.js
@@ -7,7 +7,18 @@ function    miFuncion(a,b){
 
 //llamada a la función
 miFuncion(5,4);
+console.log(miFuncion(5,4));
 
 let resultado = miFuncion(6,7);
 console.log(resultado);
+
+//declaramos otra funcion de tipo expresion o anonima
+let x = function(a,b){return a+b}; // nesecita cierre con punto y coma
+resultado = x(5,6); // al llamarla se pone la variable y parentesis
+console.log(resultado);
+
+//funciones de tipo self y invoking (no se puede reutilizar)
+(function(a,b){
+    console.log("Ejecutando la funcion: " + (a+b));
+})(9,6);
 
