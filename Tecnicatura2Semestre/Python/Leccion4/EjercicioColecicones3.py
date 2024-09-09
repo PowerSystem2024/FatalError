2c5dff411fcc4ee74e95021d4c6911a46e2c6202
Luca Perez <lucamarper@gmail.com>
2024-09-09 20:25:08 -0300

    Agregamos ejercicios python actualizados

---
+++ Tecnicatura2Semestre/Python/Leccion4/EjercicioColecicones3.py
@@ -0,0 +1,29 @@
+#Ejercicio3 : agregar personajes a una lista
+#Escriba un programa que tenga una lista de personajes de la saga de el señor de los anillos
+#nombre:Aragon
+#Clase: Guerrero
+#Raza: Dúnadan del Norte
+
+#nombre:Gandalf
+#Clase: Mago
+#Raza: Istar
+
+#nombre:Legolas
+#Clase: Arquero
+#Raza: Elfo Sindar
+
+personajes=[] #Creamos una lista vacia
+#Creamos diccionarios
+P1 = {'nombre':'Aragon','clase':'Guerrero','raza':'Dúnadan del Norte'}
+personajes.append(P1) #Agregamos el diccionario a la lista
+P2 = {'nombre':'Gandalf','clase':'Mago','raza':'Istar'}
+personajes.append(P2) #Agregamos el diccionario a la lista
+P3 = {'nombre':'Legolas','clase':'Arquero','raza':'Elfo Sindar'}
+personajes.append(P3) #Agregamos el diccionario a la lista
+P4= {'nombre':'Frodo','clase':'Hobbit','raza':'Hobbit'}
+personajes.append(P4) #Agregamos el diccionario a la lista
+P5= {'nombre':'Sam','clase':'Hobbit','raza':'Hobbit'}
+personajes.append(P5) #Agregamos el diccionario a la lista
+print(personajes) #Mostramos la lista
+
+
