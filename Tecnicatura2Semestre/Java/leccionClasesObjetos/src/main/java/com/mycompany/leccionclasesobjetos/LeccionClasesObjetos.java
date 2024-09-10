6177356f590869bc45ba194d6e891ce4ea722107
Luca Perez <lucamarper@gmail.com>
2024-09-10 15:28:42 -0300

    Agregamos las clases de leccionclases objetos de POO en java

---
+++ Tecnicatura2Semestre/Java/leccionClasesObjetos/src/main/java/com/mycompany/leccionclasesobjetos/LeccionClasesObjetos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.leccionclasesobjetos;
+
+/**
+ *
+ * @author luca
+ */
+public class LeccionClasesObjetos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
