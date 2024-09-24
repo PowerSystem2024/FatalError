ad794718a17c86a843656c7fe7f80676d62f600a
unknown <Jorgemathez14@gmail.com>
2024-09-24 15:19:42 -0300

    listta clase5

--- Tecnicatura2Semestre/Js/TSLeccion05/05-03-clases.js
+++ Tecnicatura2Semestre/Js/TSLeccion05/05-03-clases.js
@@ -1,6 +1,6 @@
 //let Persona3 = new Persona('Carla', 'Ponce'); esto no se debe hacer
 
-class Persona {
+class Persona { //clase padre
     constructor(nombre, apellido) {
         this._nombre = nombre;
         this._apellido = apellido;
@@ -24,6 +24,22 @@ class Persona {
     }
 }
 
+class Empleado extends Persona{ //clase hija
+    constructor(nombre, apellido, departamento){
+        super(nombre, apellido);
+        this._departamento = departamento;
+    }
+
+    get departamento(){
+        return this._departamento;
+
+    }
+    set departamento(departamento){
+        this._departamento = departamento;
+    }
+
+}
+
 let persona1 = new Persona('Lilieth', 'Chacon');
 console.log(persona1.nombre);
 persona1.nombre = 'juan carlos';
@@ -37,4 +53,10 @@ let persona2 = new Persona('Carlos', 'Lara');
 console.log(persona2.nombre);
 persona2.nombre = 'Maria laura';
 console.log(persona2.nombre);
+
+
+let Empleado1 = new Empleado('Maria', 'Gimenez', 'Sistemas');
+console.log(Empleado1);
+console.log(Empleado1.nombre);
+
 
