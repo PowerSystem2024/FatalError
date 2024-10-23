9b1d34e273f2feedc2e9009e194a584be5073ff3
guille <kondra.guille@gmail.com>
2024-10-22 21:09:11 -0300

    Python clase 10 pt1 del video 1 al 6

--- Tecnicatura2Semestre/Python/Leccion09/FiguraGeometrica.py
+++ Tecnicatura2Semestre/Python/Leccion09/FiguraGeometrica.py
@@ -1,6 +1,46 @@
-class FiguraGeometrica:
-    def __init__(self, alto, ancho):
-        
-        self.alto = alto
-        self.ancho = ancho
+from abc import ABC, abstractmethod #importamos clase abstracta y un decorador para definir metodos abstractos
+
+class FiguraGeometrica(ABC): #clase padre Hija de ABC
+    def __init__(self, ancho, alto): #dunder init
+        if  self._validar_valores(ancho):
+            self._ancho = ancho # encapsulamos con un guion bajo
+        else:
+            self._ancho = 0
+            print(f'Valor erroneo para el ancho: {ancho}')
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            self._alto = 0
+            print(f'Valor erroneo para el alto: {alto}')
     
+    @property # metodo get
+    def ancho(self):
+        return self._ancho
+        
+    @ancho.setter #metodo set
+    def ancho(self,ancho):
+        if self._validar_valores(ancho):
+            self._ancho = ancho
+        else:
+            print(f'valor erroneo ancho: {ancho}')
+    
+    @property
+    def alto(self):
+        return self._alto
+    
+    @alto.setter
+    def alto(self,alto):
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            print(f'valor erroneo alto: {alto}')
+
+    @abstractmethod
+    def calcular_area(self):
+        pass
+    
+    def __str__(self): # metodo dunder str, devuelve una representación en cadena
+        return f'FiguraGeometrica [Ancho: {self._ancho}, Alto: {self._alto}]'
+    
+    def _validar_valores(self,valor): #Metodo encapsulado
+        return True if 0 < valor < 10 else False
--- Tecnicatura2Semestre/Python/Leccion09/test_figura_geometrica.py
+++ Tecnicatura2Semestre/Python/Leccion09/test_figura_geometrica.py
@@ -1,11 +1,24 @@
-from Cuadrado import Cuadrado
+from Cuadrado import Cuadrado # importamos las clases
+from Rectangulo import Rectangulo
+from FiguraGeometrica import FiguraGeometrica
 
-cuadrado1 = Cuadrado(5,"azul")
-print(cuadrado1.ancho)
-print(cuadrado1.alto)
-print(f"Calculo del area del cuadrado: {cuadrado1.calcular_area()}")
+print('Creacion de objeto clase Cuadrado'.center(50, '_')) # separador
+cuadrado1 = Cuadrado(8,'Azul') #creamos el objeto
+cuadrado1.alto = 7
+#cuadrado1.ancho = 7
+#print(cuadrado1.ancho)
+#print(cuadrado1.alto)
+print(f'Calculo del área del cuadrado: {cuadrado1.calcular_area()}')
 
-# MRO = Method Resolution Order
+#MRO = METHOD RESOLUTION ORDER
+#print(Cuadrado.mro()) #nos muestra el orden en el que fuimos utilizando las clases
 
+print(cuadrado1)
+print('Creacion de objeto clase Rectangulo'.center(50,'_')) #separador
+rectangulo1 = Rectangulo(3,9,'verde')
+rectangulo1.ancho = 8
+print(f'Calculo del area del rectangulo: {rectangulo1.calcular_area()}')
+print(rectangulo1)
+
+# figura1 = FiguraGeometrica() no se puede instanciar, es abstracta
 print(Cuadrado.mro())
-
